f96b775084c9157c40e6029e19ae5bab1141a598
sjtuzc <2694824893@qq.com>
2022-11-02 15:40:36 +0800

    initial commit

--- server.py
+++ server.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
+@app.route('/codec', methods=['POST'])
+def encode():
+    print(request.data)
+    return 'w'
+
+if __name__ == '__main__':
+    app.run()
