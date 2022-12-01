8d9763bcf8f0492db7769c59d1e0ae5d8099b8b9
sjtuzc <2694824893@qq.com>
2022-12-01 22:02:16 +0800

    support online codec

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, url_for, request
+import sys, os
+sys.path.append(os.getcwd()+'\\codec\\codec')
+import codec as cd
+print(sys.path)
 
 app = Flask(__name__)
 
@@ -6,10 +10,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/codec', methods=['POST'])
-def encode():
+@app.route('/codec/<action>', methods=['POST'])
+def codec(action=None):
     print(request.data)
-    return 'w'
+    if action == 'encode':
+        return cd.svg_to_dna(request.data.decode(encoding='utf-8'))
+    else:
+        return cd.dna_to_svg(request.data.decode(encoding='utf-8'))
 
 if __name__ == '__main__':
     app.run()
